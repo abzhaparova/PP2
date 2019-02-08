24813dffd4934e1ffa7096d3ff329d75700d05a7
Aida <abzhaparova1999@gmail.com>
2019-02-08 12:57:57 +0600

    Labs

---
+++ Week 2/lesson2/lesson2/Program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson2
+{
+
+    class studet()
+        {
+        public String gpa;
+
+        }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //Directoryinfo dirInfo = new directoryinfo(@"c:\test")
+            
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+           
+        }
+    }
+}
