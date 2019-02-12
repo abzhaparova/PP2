d40fe7669032bd03162c629b2ced97983304ab22
Aida <abzhaparova1999@gmail.com>
2019-02-12 22:22:37 +0600

    adding comments to lab2=task4

--- week2/Task4/Task4/Program.cs
+++ week2/Task4/Task4/Program.cs
@@ -9,20 +9,22 @@ namespace Task4
 {
     class Program
     {
-        public static void CreateCopyDeleteFile()
+        public static void CreateCopyDeleteFile()//funtion does operations like creating file and  copying file and then deleting file 
         {
+            StreamWriter file = new StreamWriter(@"C:\Users\Aida\Desktop\pp2\PP2\week2\Task4\path\file.txt");//creating file   
+            file.WriteLine("Smile:) have a great day!");//writing something to inside of the file
+            file.Close();//closing the file
+            //we have another method of creating the file and writing something inside though below given line
+            //"C:\Users\Aida\Desktop\pp2\PP2\week2\Task4\path\file.txt","Smile:) have a great day!")
 
-            StreamWriter file = new StreamWriter(@"C:\Users\Aida\Desktop\pp2\PP2\week2\Task4\path\file.txt");   
-            file.WriteLine("Smile:) have a great day!");
-            file.Close();
-            File.Copy("C:/Users/Aida/Desktop/pp2/PP2/week2/Task4/path/file.txt", "C:/Users/Aida/Desktop/pp2/PP2/week2/Task4/path1/file.txt");
-            File.Delete("C:/Users/Aida/Desktop/pp2/PP2/week2/Task4/path/file.txt");
+            File.Copy("C:/Users/Aida/Desktop/pp2/PP2/week2/Task4/path/file.txt", "C:/Users/Aida/Desktop/pp2/PP2/week2/Task4/path1/file.txt");//copying the file to another directory
+            File.Delete("C:/Users/Aida/Desktop/pp2/PP2/week2/Task4/path/file.txt");//deleting the file
         }
 
         static void Main(string[] args)
         {
-            CreateCopyDeleteFile();
-            Console.ReadKey();
+            CreateCopyDeleteFile();//recalling this function
+            Console.ReadKey();//pausing the system
         }
     }
 }
