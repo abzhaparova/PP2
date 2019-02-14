ba2dc1713155dbc58c059f3a4ec9c2d230389acc
Aida <abzhaparova1999@gmail.com>
2019-02-15 04:40:07 +0600

    added some more comments for lab3=task1

--- week3/Task1/Task1/Program.cs
+++ week3/Task1/Task1/Program.cs
@@ -10,20 +10,21 @@ namespace Task1
         class Program//created class Program(it works as FarManager)
         {
             static string path = @"C:\Users\Aida\Desktop\pp2\PP2\week3\Task1\need";//directory of the path 
-            static FileSystemInfo currentFSI;
+            static FileSystemInfo currentFSI;//current item
             static int selectedindex = 0; 
             static Stack<string> hist = new Stack<string>();//creating stack for saving strings(saving layers)
 
 
         static void Show()
         {
-            Console.SetCursorPosition(0, 0);// cursors position where cursor is equal 0, because it's startpoint from 0 
+            Console.SetCursorPosition(0, 0);// (column and row)cursors position where cursor is equal 0, because it's startpoint from 0 
             DirectoryInfo d = new DirectoryInfo(@path);
 
-                List<FileSystemInfo> li = new List<FileSystemInfo>();
-                li.AddRange(d.GetDirectories());//adding directories
-                li.AddRange(d.GetFiles());//adding files
-                FileSystemInfo[] fsi = li.ToArray();//converting to array fsi because list is not mutable 
+                List<FileSystemInfo> li = new List<FileSystemInfo>();//List is used to show first folders then filesв
+                li.AddRange(d.GetDirectories());//adds elements of this range to the end of the list
+                li.AddRange(d.GetFiles());//adds elements of this range to the end of the list 
+
+            FileSystemInfo[] fsi = li.ToArray();//converting the elemets of the list to the array fsi because list is not mutable 
                 currentFSI = fsi[selectedindex];//the index of the selected item, at the start is 0
 
                 for (int i = 0; i < fsi.Length; i++)
@@ -38,8 +39,9 @@ namespace Task1
                         Console.BackgroundColor = ConsoleColor.Black;
                     }
 
-                    if (fs.GetType() == typeof(DirectoryInfo))//the condition works if the selected item is directory, so the foreground color is white, else if it is file then yellow 
-                    {
+                    if (fs.GetType() == typeof(DirectoryInfo))//if it is folder then colour differs from file
+                    //the condition works if the selected item is directory, so the foreground color is white, else if it is file then yellow 
+                {
                         Console.ForegroundColor = ConsoleColor.White;
                     }
                     else
@@ -53,11 +55,11 @@ namespace Task1
 
             static void Main(string[] args)
             {
-                while (true)//infinity loop
+                while (true)//infinite loop
                 {
                     Show();//recalling the show function
-                    ConsoleKeyInfo cki = Console.ReadKey();//считывает то что мы нажимаем на клавише 
-                    switch (cki.Key)//same as if else 
+                    ConsoleKeyInfo cki = Console.ReadKey();//variable to work withe keys
+                switch (cki.Key)//same as if else 
                     //considering possible cases that we need for Farmanager
                     {
                         case ConsoleKey.UpArrow:
@@ -69,8 +71,8 @@ namespace Task1
                         case ConsoleKey.Enter:
                             OpenFile();
                             break;
-                        case ConsoleKey.Escape:
-                            Escape();
+                        case ConsoleKey.Backspace:
+                        Backspace();
                             break;
                         case ConsoleKey.Delete:
                             Delete();
@@ -108,20 +110,20 @@ namespace Task1
                 else//if its file we will open content of the file
                 {
                     FileStream fs = new FileStream(currentFSI.FullName, FileMode.Open, FileAccess.Read);
-                    StreamReader sr = new StreamReader(fs);//reading the file through fs 
+                    StreamReader sr = new StreamReader(fs);
                     Console.WriteLine(sr.ReadToEnd());//writeline in console all readed content of the file 
-                    Console.ReadKey();//pausing the terminal
+                    Console.ReadKey();
                     sr.Close();
                     fs.Close();
                 }
                 Console.Clear();
             }
 
-            static void Escape()//function for going back
+            static void Backspace()//function for going back
             {
-                path = hist.Peek();
-                hist.Pop();
-                selectedindex = 0;
+                path = hist.Peek();//path of last folder, show запустится из родительской папки
+            hist.Pop();//delete the top element of the Stack
+                 selectedindex = 0;
                 Console.Clear();
             }
 
@@ -142,9 +144,9 @@ namespace Task1
             static void Rename()//function for renaming the file/directory
             {
                 Console.SetCursorPosition(10, 30);
-                Console.Write("Enter new name:");//writing in console for user
-                string path = currentFSI.FullName;
-                string pr = new DirectoryInfo(path).Parent.FullName; 
+                Console.Write("Enter new name:");
+                string path = currentFSI.FullName;//path of the chosen item
+                string pr = new DirectoryInfo(path).Parent.FullName; //pr->переменная, которая берет путь до название объекта
                 string newName = Console.ReadLine();// newname that user entered
 
             if (currentFSI.GetType() == typeof(FileInfo))
